d573c479c54856a93eeca9f85b79f16e18d747ca
pavan6476252 <meesalapavan2003@gmail.com>
2024-05-12 22:45:07 +0530

    added exception handling

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -47,67 +47,83 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email }).select("+password");
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "Register first",
-    });
-  }
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Register first",
+      });
+    }
 
-  const encrypted = await bcrypt.compare(password, user.password);
-  if (!encrypted) {
-    return res.status(404).json({
-      success: false,
-      message: "your password is incorrect",
-    });
-  }
+    const encrypted = await bcrypt.compare(password, user.password);
+    if (!encrypted) {
+      return res.status(404).json({
+        success: false,
+        message: "your password is incorrect",
+      });
+    }
 
-  sendcookie(user, res, `welcome back,${user.name}`, 201);
+    sendcookie(user, res, `welcome back,${user.name}`, 201);
+  } catch (e) {
+    throw next(e);
+  }
 };
 
-export const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({
-      success: false,
-      message: "user already exists",
-    });
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "user already exists",
+      });
+    }
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+    user = await User.create({ name, email, password: hashedpassword });
+
+    sendcookie(user, res, "successfully registered", 201);
+  } catch (e) {
+    throw next(e);
   }
-
-  const hashedpassword = await bcrypt.hash(password, 10);
-  user = await User.create({ name, email, password: hashedpassword });
-
-  sendcookie(user, res, "successfully registered", 201);
 };
 
-export const getMyprofile = async (req, res) => {
-  // 2nd method by {Abhisekhsuru}
-  // const { id } = req.query;
-  // const user = await User.findById(id);
-  //without authentication
-  const user = await User.findById(req.user._id);
-  res.status(200).json({
-    success: true,
-    message: `your profile is found ${req.user.name}`,
-    user: user,
-  });
-};
-
-export const logout = (req, res) => {
-  res
-    .cookie("token", null, {
-      expires: new Date(0),
-      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "development" ? false : true,
-    })
-    .status(200)
-    .json({
+export const getMyprofile = async (req, res, next) => {
+  try {
+    // 2nd method by {Abhisekhsuru}
+    // const { id } = req.query;
+    // const user = await User.findById(id);
+    //without authentication
+    const user = await User.findById(req.user._id);
+    res.status(200).json({
       success: true,
-      message: "Successfully logout",
-      user: req.user,
+      message: `your profile is found ${req.user.name}`,
+      user: user,
     });
+  } catch (e) {
+    throw next(e);
+  }
+};
+
+export const logout = async (req, res, next) => {
+  try {
+    res
+      .cookie("token", null, {
+        expires: new Date(0),
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "development" ? false : true,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully logout",
+        user: req.user,
+      });
+  } catch (e) {
+    throw next(e);
+  }
 };
--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,48 +4,64 @@ import { isAuthenticated } from "../middlewares/user_auth.js";
 
 const companyRouter = express.Router();
 
-companyRouter.get("/", async (req, res) => {
-  const locations = await CompanyModel.find()
-  .populate("createdBy")
-  .populate("editedBy");
+companyRouter.get("/", async (req, res, next) => {
+  try {
+    const locations = await CompanyModel.find()
+      .populate("createdBy")
+      .populate("editedBy");
 
-  return res.status(200).json(locations || []);
+    return res.status(200).json(locations || []);
+  } catch (e) {
+    throw next(e);
+  }
 });
-companyRouter.get("/:id", async (req, res) => {
-  const locations = await CompanyModel.findById(req.params.id)
-    .populate("createdBy")
-    .populate("editedBy");
+companyRouter.get("/:id", async (req, res, next) => {
+  try {
+    const locations = await CompanyModel.findById(req.params.id)
+      .populate("createdBy")
+      .populate("editedBy");
 
-  return res.status(200).json(locations);
+    return res.status(200).json(locations);
+  } catch (e) {
+    throw next(e);
+  }
 });
 
-companyRouter.patch("/:id", isAuthenticated, async (req, res) => {
-  const { name, description } = req.body;
-  const editedBy = req.user._id;
+companyRouter.patch("/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const editedBy = req.user._id;
 
-  await CompanyModel.findByIdAndUpdate(req.params.id, {
-    $set: {
+    await CompanyModel.findByIdAndUpdate(req.params.id, {
+      $set: {
+        name,
+        description,
+        editedBy,
+      },
+    });
+
+    return res.status(200).json({ message: "Success" });
+  } catch (e) {
+    throw next(e);
+  }
+});
+
+companyRouter.post("/", isAuthenticated, async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const user_id = req.user._id;
+
+    const location = new CompanyModel({
+      createdBy: user_id,
       name,
       description,
-      editedBy,
-    },
-  });
+    });
 
-  return res.status(200).json({ message: "Success" });
-});
-
-companyRouter.post("/", isAuthenticated, async (req, res) => {
-  const { name, description } = req.body;
-  const user_id = req.user._id;
-
-  const location = new CompanyModel({
-    createdBy: user_id,
-    name,
-    description,
-  });
-
-  await location.save();
-  return res.status(200).json({ message: "Success" });
+    await location.save();
+    return res.status(200).json({ message: "Success" });
+  } catch (e) {
+    throw next(e);
+  }
 });
 
 export default companyRouter;
