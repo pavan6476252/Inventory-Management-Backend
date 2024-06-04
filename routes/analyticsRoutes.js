61641e65513f230f8ecaa9ba380ec213eb5e2ac0
Pavan Kumar Meesala <63488643+pavan6476252@users.noreply.github.com>
2024-06-04 17:17:27 +0530

    Update analyticsRoutes.js

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import ProductModel from "../models/product_model.js";
 import HistoryModel from "../models/history_model.js";
+import Product from "../models/product_model.js";
 
 const analyticsRoutes = express.Router();
 
+// Route to get expiring products
 analyticsRoutes.get("/expiring", async (req, res) => {
   try {
-    const { months = 1 } = req.query; // Default to 1 month if not specified
+    const { months = 0 } = req.query; // Default to 1 month if not specified
     const expiringProducts = await getExpiringProducts(parseInt(months));
     res.status(200).json(expiringProducts);
   } catch (error) {
@@ -16,12 +18,27 @@ analyticsRoutes.get("/expiring", async (req, res) => {
 
 // Function to get expiring products
 const getExpiringProducts = async (months) => {
-  const currentDate = new Date();
-  const expirationDate = new Date(currentDate);
-  expirationDate.setMonth(expirationDate.getMonth() + months);
+  const futureDate = new Date();
+  futureDate.setMonth(futureDate.getMonth() + months);
 
-  const expiringProducts = await ProductModel.find({
-    dateOfPurchase: { $lte: expirationDate }, // Products purchased before expirationDate
+  const expiringProducts = await Product.find({
+    $and: [
+      {
+        $expr: {
+          $lte: [
+            {
+              $add: [
+                "$dateOfPurchase",
+                {
+                  $multiply: ["$warrantyMonths", 30 * 24 * 60 * 60 * 1000], // Convert warranty months to milliseconds
+                },
+              ],
+            },
+            futureDate,
+          ],
+        },
+      },
+    ],
   })
     .populate("createdBy")
     .populate({
@@ -68,24 +85,38 @@ const getProductUsageByUser = async () => {
 
 // Function to get warranty status
 const getWarrantyStatus = async () => {
-  const result = await ProductModel.aggregate([
+  const currentDate = new Date();
+  
+  const result = await Product.aggregate([
     {
       $project: {
         status: {
           $cond: [
-            { $lte: ["$dateOfPurchase", new Date()] },
+            {
+              $gte: [
+                {
+                  $add: [
+                    "$dateOfPurchase",
+                    {
+                      $multiply: ["$warrantyMonths", 30 * 24 * 60 * 60 * 1000] // Convert warranty months to milliseconds
+                    }
+                  ]
+                },
+                currentDate
+              ]
+            },
             "in warranty",
-            "not in warranty",
-          ],
-        },
-      },
+            "not in warranty"
+          ]
+        }
+      }
     },
     {
       $group: {
         _id: "$status",
-        count: { $sum: 1 },
-      },
-    },
+        count: { $sum: 1 }
+      }
+    }
   ]);
 
   const labels = result.map((item) => item._id);
